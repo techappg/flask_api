a4f059c2ba48eb4745bd90b30f400ff7d6af80cb
Shidhant Thapa <shidhantt67@gmail.com>
2021-01-18 19:04:08 +0530

    changes

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class B2CPayment(Resource):
     def get(self):
         token = GetToken().get()
         print("token : ", token.get("access_token"))
-        url = "https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest"
+        url = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest"
 
         payload = {
             "InitiatorName": "apiop37",
@@ -56,9 +56,37 @@ class B2CPayment(Resource):
 
         return json.loads(response.text)
 
+class MpaisaTransactionStatus(Resource):
+    def get(self):
+        token = GetToken().get()
+        print("token : ", token.get("access_token"))
+        url = "https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query"
+
+        payload = {
+            "Initiator": "apiop37",
+            "SecurityCredential": "bgTn7e054OhXe6zoP5pUhKRjrnZ69AWgVnhjU7n5SPU9loNw7vGgGqB5X0TZN6LzQGpSIDFsCHOh6CzsCdcnzj1AlDouzqydU1xBaH1z+ak1BZKq7LsHc69r+qtyZ/VAxOS+PQRBUVBXUAtPQRMTzeDq3q87TIRpzZdqA8eJYL3QpfoD4Rteg4iYH4+iMgR7AVTDuYQgLpQNx6SQSIZVFKI5RqeaHH/FMdjbqaJ6mCuT2EC6XlcBu49RWSHtLP3Y5oScwmuFXV8oQp+1bz5NXm/llJvQsL6npmRyzXB69S3TZZnUNHk4OsJkZ9cfpWAaGdrrt8uqIn9o2SNIR0mZnQ==",
+            "CommandID": "TransactionStatusQuery",
+            "TransactionID": "PA671HI7SL",
+            "PartyA": "603021",
+            "IdentifierType": "4",
+            "ResultURL": "https://webhook.site/20d56f33-eafa-4ca8-8084-17e749e09d5d",
+            "QueueTimeOutURL": "https://webhook.site/20d56f33-eafa-4ca8-8084-17e749e09d5d",
+            "Remarks": "test"
+        }
+        headers = {
+            'Authorization': f'Bearer {token.get("access_token")}',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+
+        return json.loads(response.text)
+
+
 api.add_resource(Index, '/')
 api.add_resource(GetToken, '/token')
 api.add_resource(B2CPayment, '/b2c-payment')
+api.add_resource(MpaisaTransactionStatus, '/transaction-status')
 
 if __name__ == '__main__':
     app.run(debug=True)
